a9ecdbdf591fbec8927610e43f81dfca4642e3f2
AlexGyver <alexgyvershow@ya.ru>
2026-03-14 14:13:13 +0300

    upd 3.0

--- bson.js
+++ bson.js
@@ -1,230 +1,283 @@
-const BS_STRING = (0 << 5);
-const BS_BOOLEAN = (1 << 5);
-const BS_INTEGER = (2 << 5);
-const BS_FLOAT = (3 << 5);
-const BS_CODE = (4 << 5);
-const BS_BINARY = (5 << 5);
-const BS_CONTAINER = (6 << 5);
-const BS_NULL = (7 << 5);
+//#region Const
+// type
+const BS_TYPE_MASK = 0b11000000;
+const BS_SUBT = 0 << 6;
+const BS_INT = 1 << 6;
+const BS_STR = 2 << 6;
+const BS_CODE = 3 << 6;
 
-const BS_CONT_OBJ = (1 << 4);
-const BS_CONT_OPEN = (1 << 3);
-const BS_CONT_ARR = (1 << 2);
-const BS_CONT_CLOSE = (1 << 1);
+// subtype
+const BS_SUBT_MASK = 0b00111000;
+const BS_NULL = 0 << 3;
+const BS_BOOL = 1 << 3;
+const BS_CONT = 2 << 3;
+const BS_FLOAT = 3 << 3;
+const BS_BIN = 4 << 3;
 
-const BS_MAX_LEN = 0b0001111111111111;
+// type int
+const BS_INT_SMALL = 1 << 5;
+const BS_INT_NEG = 1 << 4;
+const BS_GET_SMALLV = x => x & 0b11111;
+const BS_GET_INT_LEN = x => x & 0b1111;
 
-const BS_TYPE_MASK = 0b11100000;
-const BS_TYPE = x => x & BS_TYPE_MASK;
-const BS_DATA_MASK = 0b00011111;
-const BS_DATA = x => x & BS_DATA_MASK;
-
-const BS_BOOLV_MASK = 0b1;
-const BS_BOOLV = x => x & BS_BOOLV_MASK;
-
-const BS_NEG_MASK = 0b10000;
-const BS_NEGATIVE = x => x & BS_NEG_MASK;
-const BS_SIZE_MASK = 0b01111;
-const BS_SIZE = x => x & BS_SIZE_MASK;
-
-const BS_FLOAT_SIZE = 4;
-
-const BS_D16_MSB = x => (x >> 8) & BS_DATA_MASK;
-const BS_D16_LSB = x => x & 0xff;
-const BS_D16_MERGE = (msb5, lsb) => ((msb5 << 8) | lsb) >>> 0;
-
+// type string
+// type code
+const BS_EXT_BITS = 5;
+const BS_EXT_FLAG = 1 << BS_EXT_BITS;
+const BS_EXT_MASK = BS_EXT_FLAG - 1;
+const BS_MAX_EXT_LEN = (1 << (BS_EXT_BITS + 8)) - 1;
+const BS_GET_LSB = x => x & BS_EXT_MASK;
+const BS_GET_MSB = x => x >> BS_EXT_BITS;
 const BS_CODE_PREFIX = "__BS#";
 
+// subtype cont
+const BS_CONT_ARR = 0 << 0;
+const BS_CONT_OBJ = 1 << 0;
+const BS_CONT_CLOSE = 0 << 1;
+const BS_CONT_OPEN = 1 << 1;
+
+// subtype float
+const BS_GET_FLOAT_LEN = x => 1 << ((x) & 0b111);
+const BS_FLOAT_F32 = 2;
+
+// subtype bin
+const BS_GET_BIN_LEN = x => x & 0b111;
+
+//#region BSDecoder
+export class BSDecoder {
+    offset = 0;
+    cont = 'root';
+    tdec = new TextDecoder();
+
+    constructor(buf, codes) {
+        this.buf = buf;
+        this.codes = codes;
+    }
+
+    decode() {
+        if (!(this.buf instanceof Uint8Array) || !this.buf.length) return undefined;
+
+        let res = this._decode();
+        if (this.offset != this.buf.length) throw this._err("Broken packet");
+        return res;
+    }
+    read(len) {
+        if (this.offset + len > this.buf.length) {
+            throw this._err("Overflow");
+        }
+        let res = this.buf.subarray(this.offset, this.offset + len);
+        this.offset += len;
+        return res;
+    }
+    readB() {
+        return this.read(1)[0];
+    }
+    readInt(len) {
+        let val = 0;
+        let bytes = this.read(len);
+        while (len--) val = (val << 8) | bytes[len];
+        return val;
+    }
+    _ext(hdr) {
+        return BS_GET_LSB(hdr) | ((hdr & BS_EXT_FLAG) ? (this.readB() << BS_EXT_BITS) : 0);
+    }
+    _close(res, isObj) {
+        if (res && res.__close !== undefined) {
+            if (isObj === res.__close) return true;
+            throw this._err("Wrong close");
+        }
+        return false;
+    }
+    _err(e) {
+        return new Error(e + ' in ' + JSON.stringify(this.cont));
+    }
+    _decode() {
+        const header = this.readB();
+
+        let type = header & BS_TYPE_MASK;
+        if (type == BS_SUBT) type = header & BS_SUBT_MASK;
+
+        switch (type) {
+            case BS_CONT:
+                if (header & BS_CONT_OPEN) {
+                    let isObj = !!(header & BS_CONT_OBJ);
+                    let cont = isObj ? {} : [];
+
+                    while (true) {
+                        this.cont = cont;
+
+                        let res = this._decode();
+                        if (this._close(res, isObj)) return cont;
+
+                        if (isObj) {
+                            let val = this._decode();
+                            if (this._close(val, isObj)) throw this._err("Missed value");
+
+                            cont[String(res)] = val;
+                        } else {
+                            cont.push(res);
+                        }
+                    }
+                }
+                return { __close: !!(header & BS_CONT_OBJ) };
+
+            case BS_INT:
+                if (header & BS_INT_SMALL) {
+                    return BS_GET_SMALLV(header);
+                } else {
+                    let val = this.readInt(BS_GET_INT_LEN(header));
+                    return (header & BS_INT_NEG) ? -val : val;
+                }
+
+            case BS_STR:
+                return this.tdec.decode(this.read(this._ext(header)));
+
+            case BS_CODE:
+                return this.codes[this._ext(header)];
+
+            case BS_NULL:
+                return null;
+
+            case BS_BOOL:
+                return !!(header & 1);
+
+            case BS_FLOAT: {
+                let len = BS_GET_FLOAT_LEN(header);
+                const b = this.read(len);
+                let view = new DataView(b.buffer, b.byteOffset, len);
+                return (len == 4) ? view.getFloat32(0, true) : ((len == 8) ? view.getFloat64(0, true) : 0);
+            }
+
+            case BS_BIN:
+                return this.read(this.readInt(BS_GET_BIN_LEN(header))).slice();
+        }
+    }
+}
+
+//#region BSEncoder
+export class BSEncoder {
+    arr = [];
+    tenc = new TextEncoder();
+
+    constructor(codes = []) {
+        this.codes = codes;
+    }
+
+    getArray() {
+        return Uint8Array.from(this.arr);
+    }
+
+    encode(val) {
+        let a = this.arr;
+
+        if (Array.isArray(val)) {
+            a.push(BS_SUBT | BS_CONT | BS_CONT_ARR | BS_CONT_OPEN);
+            val.forEach(v => this.encode(v));
+            a.push(BS_SUBT | BS_CONT | BS_CONT_ARR | BS_CONT_CLOSE);
+        } else {
+            switch (typeof val) {
+                case 'object':
+                    if (val === null) {
+                        a.push(BS_SUBT | BS_NULL);
+                    } else if (val instanceof Uint8Array) {
+                        let lenb = this._int(val.length);
+                        a.push(BS_SUBT | BS_BIN | lenb.length);
+                        a.push(...lenb);
+                        a.push(...val);
+                    } else {
+                        a.push(BS_SUBT | BS_CONT | BS_CONT_OBJ | BS_CONT_OPEN);
+                        for (const [key, value] of Object.entries(val)) {
+                            this.encode(key);
+                            this.encode(value);
+                        }
+                        a.push(BS_SUBT | BS_CONT | BS_CONT_OBJ | BS_CONT_CLOSE);
+                    }
+                    break;
+
+                case 'number':
+                    if (Number.isInteger(val)) {
+                        if (val >= 0 && val < BS_INT_SMALL) {
+                            a.push(BS_INT | BS_INT_SMALL | val);
+                        } else {
+                            let neg = val < 0;
+                            if (neg) val = -val;
+                            let bytes = this._int(val);
+                            a.push(BS_INT | (neg ? BS_INT_NEG : 0) | bytes.length);
+                            a.push(...bytes);
+                        }
+                    } else {
+                        const buffer = new ArrayBuffer(4);
+                        new DataView(buffer).setFloat32(0, val, true);
+                        a.push(BS_SUBT | BS_FLOAT | BS_FLOAT_F32);
+                        a.push(...new Uint8Array(buffer));
+                    }
+                    break;
+
+                case 'string':
+                    if (val.startsWith(BS_CODE_PREFIX)) {
+                        val = val.slice(BS_CODE_PREFIX.length);
+                        let code = this.codes.indexOf(val);
+                        if (code >= 0) this._ext(BS_CODE, code);
+                        else this.encode(val);
+                    } else {
+                        const bytes = this.tenc.encode(val);
+                        const len = Math.min(bytes.length, BS_MAX_EXT_LEN);
+                        this._ext(BS_STR, len);
+                        a.push(...bytes.slice(0, len));
+                    }
+                    break;
+
+                case 'boolean':
+                    a.push(BS_SUBT | BS_BOOL | !!val);
+                    break;
+
+                default:
+                    a.push(BS_SUBT | BS_NULL);
+                    break;
+            }
+        }
+
+        return this;
+    }
+
+    _int(v) {
+        let t = [];
+        while (v) {
+            t.push(v & 0xFF);
+            v >>= 8;
+        }
+        return t;
+    }
+
+    _ext(t, v) {
+        let a = this.arr;
+        if (v < BS_EXT_FLAG) {
+            a.push(t | v);
+        } else {
+            a.push(t | BS_EXT_FLAG | BS_GET_LSB(v));
+            a.push(BS_GET_MSB(v));
+        }
+    }
+}
+
+//#region helper
+
 /**
- * @param {Uint8Array} b
+ * @param {Uint8Array} buf
  * @param {Array} codes
  * @returns {Object|Array|*}
  */
 export function decodeBson(buf, codes = []) {
-    if (!(buf instanceof Uint8Array)) return undefined;
-
-    const reader = {
-        buf,
-        offset: 0,
-        decoder: new TextDecoder(),
-        read(len) {
-            if (this.offset + len > buf.length) {
-                throw new Error("Overflow");
-            }
-            const res = this.buf.subarray(this.offset, this.offset + len);
-            this.offset += len;
-            return res;
-        },
-        readB() {
-            return this.read(1)[0];
-        }
-    };
-
-    return _decode(reader, codes);
-}
-
-function _decode(r, codes) {
-    const header = r.readB();
-    const data = BS_DATA(header);
-
-    switch (BS_TYPE(header)) {
-        case BS_CONTAINER:
-            if (data & BS_CONT_OPEN) {
-                let isArr = !!(data & BS_CONT_ARR);
-                let cont = isArr ? [] : {};
-                let expect = false;
-                let key;
-
-                while (true) {
-                    let res = _decode(r, codes);
-
-                    if (res && res.__close !== undefined) {
-                        if (isArr === res.__close) {
-                            if (!isArr && expect) throw new Error("Missed value: " + JSON.stringify(cont));
-                            return cont;
-                        } else {
-                            throw new Error("Wrong close: " + JSON.stringify(cont));
-                        }
-                    }
-
-                    if (isArr) {
-                        cont.push(res);
-                    } else {
-                        if (expect) cont[key] = res;
-                        else key = res;
-                        expect = !expect;
-                    }
-                }
-            } else if (data & BS_CONT_CLOSE) {
-                return { __close: !!(data & BS_CONT_ARR) };
-            } else {
-                throw new Error("Unknown cont: " + JSON.stringify(cont));
-            }
-        // break;
-
-        case BS_CODE:
-            return codes[BS_D16_MERGE(data, r.readB())];
-
-        case BS_STRING: {
-            let len = BS_D16_MERGE(data, r.readB());
-            return r.decoder.decode(r.read(len));
-        }
-
-        case BS_INTEGER: {
-            let size = BS_SIZE(data);
-            if (!size) return 0;
-
-            let value = 0n;
-            const bytes = r.read(size);
-            while (size--) value = (value << 8n) | BigInt(bytes[size]);
-
-            if (BS_NEGATIVE(data)) value = -value;
-            return (value >= Number.MIN_SAFE_INTEGER && value <= Number.MAX_SAFE_INTEGER) ? Number(value) : value;
-        }
-
-        case BS_BOOLEAN:
-            return !!BS_BOOLV(data);
-
-        case BS_NULL:
-            return null;
-
-        case BS_FLOAT: {
-            const b = r.read(4);
-            return new DataView(b.buffer, b.byteOffset, 4).getFloat32(0, true);
-        }
-
-        case BS_BINARY: {
-            let len = BS_D16_MERGE(data, r.readB());
-            return r.read(len).slice();
-        }
-    }
+    return new BSDecoder(buf, codes).decode();
 }
 
 /**
  * @param {*} val
+ * @param {Array} codes
  * @returns {Uint8Array}
  */
 export function encodeBson(val, codes = []) {
-    let arr = [];
-    _encode(val, arr, codes);
-    return Uint8Array.from(arr);
-}
-
-function _encode(val, arr, codes) {
-    if (Array.isArray(val)) {
-        arr.push(BS_CONTAINER | BS_CONT_ARR | BS_CONT_OPEN);
-        val.forEach(v => _encode(v, arr, codes));
-        arr.push(BS_CONTAINER | BS_CONT_ARR | BS_CONT_CLOSE);
-        return;
-    }
-
-    switch (typeof val) {
-        case 'object':
-            if (val === null) {
-                arr.push(BS_NULL);
-            } else if (val instanceof Uint8Array) {
-                const len = Math.min(val.length, BS_MAX_LEN);
-                arr.push(BS_BINARY | BS_D16_MSB(len));
-                arr.push(BS_D16_LSB(len));
-                arr.push(...val.slice(0, len));
-            } else {
-                arr.push(BS_CONTAINER | BS_CONT_OBJ | BS_CONT_OPEN);
-                for (const [key, value] of Object.entries(val)) {
-                    _encode(key, arr, codes);
-                    _encode(value, arr, codes);
-                }
-                arr.push(BS_CONTAINER | BS_CONT_OBJ | BS_CONT_CLOSE);
-            }
-            break;
-
-        case 'bigint':
-        case 'number':
-            if (Number.isInteger(val)) {
-                val = BigInt(val);
-                const neg = val < 0;
-                if (neg) val = -val;
-                let bytes = [];
-                while (val) {
-                    bytes.push(Number(val & 0xFFn));
-                    val >>= 8n;
-                }
-                arr.push(BS_INTEGER | (neg ? BS_NEG_MASK : 0) | bytes.length);
-                arr.push(...bytes);
-            } else {
-                const buffer = new ArrayBuffer(BS_FLOAT_SIZE);
-                new DataView(buffer).setFloat32(0, val, true);
-                arr.push(BS_FLOAT);
-                arr.push(...new Uint8Array(buffer));
-            }
-            break;
-
-        case 'string':
-            if (val.startsWith(BS_CODE_PREFIX)) {
-                val = val.slice(BS_CODE_PREFIX.length);
-                let code = codes.indexOf(val);
-                if (code >= 0) {
-                    arr.push(BS_CODE | BS_D16_MSB(code));
-                    arr.push(BS_D16_LSB(code));
-                } else {
-                    _encode(val, arr, codes);
-                }
-            } else {
-                const bytes = new TextEncoder().encode(val);
-                const len = Math.min(bytes.length, BS_MAX_LEN);
-                arr.push(BS_STRING | BS_D16_MSB(len));
-                arr.push(BS_D16_LSB(len));
-                arr.push(...bytes.slice(0, len));
-            }
-            break;
-
-        case 'boolean':
-            arr.push(BS_BOOLEAN | !!val);
-            break;
-
-        default:
-            arr.push(BS_NULL);
-            break;
-    }
+    return new BSEncoder(codes).encode(val).getArray();
 }
 
 export function getCode(name) {
